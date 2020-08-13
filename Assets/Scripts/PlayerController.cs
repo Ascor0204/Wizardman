e6042c5d89a7114746c9a813da6ce2a1d0d523ba
Xi Zhu <xizhu0204@gmail.com>
2020-08-13 12:50:26 -0800

    Added floor tile, coded transitions into walk and jump, simulated gravity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    bool gameStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && gameStarted == false) //game start if (checks for space press)
+        {
+            gameStarted = true;
+            anim.SetTrigger("start");
+            return;
+        }//end of game start if
+
+        if (Input.GetKeyDown(KeyCode.Space)) //start jump if (checks for space press)
+        {
+            anim.SetTrigger("jump");
+        } //end jump if
+    }
+}
