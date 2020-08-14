acc1d8c00031f9bcf2da682a7cb9300ce00d2843
Xi Zhu <xizhu0204@gmail.com>
2020-08-14 15:05:16 -0500

    Added background and jump animation triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,32 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    bool gameStarted = false;
+
     public Animator anim;
     public Rigidbody2D rb;
 
     public int jumpForce;
 
-    bool gameStarted = false;
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
+
+    public float yVelocity;
+    
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && gameStarted == false) //game start if (checks for space press)
+        if (gameStarted == false && Input.GetKeyDown(KeyCode.Space)) //game start if (checks for space press)
         {
             gameStarted = true;
             anim.SetTrigger("start");
             return;
         }//end of game start if
 
-        if (Input.GetKeyDown(KeyCode.Space)) //start jump if (checks for space press)
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) //start jump if (checks for space press)
         {
             anim.SetTrigger("jump");
             rb.AddForce(Vector2.up * jumpForce);
         } //end jump if
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("grounded", grounded);
     }
 }
