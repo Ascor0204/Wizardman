358634e77db1cdb3b65e1408575dfe9c4ec8da51
Xi Zhu <xizhu0204@gmail.com>
2020-08-19 03:16:41 -0500

    Set up spawns for fireballs and countdown to when they appear

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+
+    public GameObject green;
+    public GameObject purple;
+
+    bool appeared = false;
+
+    public float countdown = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameObject.Find("Wizardman").GetComponent<PlayerController>().gameStarted)
+        { //start if (pauses move background during idle
+            return;
+        } //end if
+
+        if (!appeared)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown < 0)
+        {
+            appeared = true;
+            green.SetActive(true);
+            purple.SetActive(true);
+        }
+
+
+    }
+}
