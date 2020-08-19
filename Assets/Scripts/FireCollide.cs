6dc1e847fc82da5e8224643978097a6cf432ee31
Xi Zhu <xizhu0204@gmail.com>
2020-08-19 15:06:09 -0500

    Fire collision

---
+++ Assets/Scripts/FireCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCollide : MonoBehaviour
+{
+
+    
+    public Transform playerPosit;
+    public Transform firePosit;
+
+    public GameObject fire;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.instance.player.GameOver();
+            fire.SetActive(false);
+        }
+
+    }
+}
